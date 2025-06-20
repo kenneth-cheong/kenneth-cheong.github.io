58a6b72a4b004451d9039d0ffc6eca3e03258f91
Kenneth Cheong <clarinet.kenneth@gmail.com>
2025-06-20 15:37:32 +0800

    .

--- kaiadmin-lite-1.2.0/assets/js/demo.js
+++ kaiadmin-lite-1.2.0/assets/js/demo.js
@@ -189,20 +189,6 @@ Circles.create({
 	styleText:    true
 })
 
-//Notify
-$.notify({
-	icon: 'icon-bell',
-	title: 'Kaiadmin',
-	message: 'Premium Bootstrap 5 Admin Dashboard',
-},{
-	type: 'secondary',
-	placement: {
-		from: "bottom",
-		align: "right"
-	},
-	time: 1000,
-});
-
 // Jsvectormap
 var world_map = new jsVectorMap({
 	selector: "#world-map",
