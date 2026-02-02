4c61a0a021aeeb187b689ba995bce8e3e669062f
Kenneth Cheong <clarinet.kenneth@gmail.com>
2026-02-02 18:31:07 +0800

    Update dataforseocrawler.py

--- dataforseocrawler.py
+++ dataforseocrawler.py
@@ -97,7 +97,7 @@ def lambda_handler(event, context):
             return {
                 'statusCode': 200,
                 'headers': {'Access-Control-Allow-Origin': '*'},
-                'body': json.dumps(response.json())
+                'body': response.json()
             }
 
         return {
